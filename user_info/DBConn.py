4dea66e8b0a1c750a9e5f9efcfb86b98fd3b3c95
je0nh <winter0pear@gmail.com>
2024-01-25 16:33:19 +0900

    refactor: Separate DB connection and Top 10000 table loading into functions

---
+++ user_info/DBConn.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.types import Integer, VARCHAR, CHAR, DECIMAL, BIGINT
+import pandas as pd
+
+def db_conn(db, password, db_ip):
+    db_path = f'{db}:{password}@{db_ip}:3306/sof'
+
+    engine = create_engine(f'mysql://{db_path}')
+    
+    try:
+        # 연결 시도
+        with engine.connect():
+            print("Database connection successful.")
+    except OperationalError as e:
+        print(f"Database connection failed. Error: {e}")
+    
+    return engine
+
+def top10000_db(top10000_path, engine):
+    df = pd.read_parquet(top10000_path)
+    df = df[df.notnull().all(axis=1)]
+
+    # price는 'BP'라고 들어간 것이 있어서 VARCHAR로
+    dtype = {
+        'rankNo' : Integer(),
+        'LV' : Integer(),
+        'nickName' : VARCHAR(20),
+        'rankScore' : DECIMAL(precision=6, scale=2),
+        'tier' : CHAR(6),
+        'korRankTier' : VARCHAR(7),
+        'price' : VARCHAR(20),
+        'ouid' : CHAR(32)
+    }
+
+    df.to_sql(name='top10000', con=engine, index=False, dtype=dtype, if_exists='replace')
+    print('Data loading completed..')
